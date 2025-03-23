c5f2aea33252e9029e7923aefe08fbc98626e6aa
Angelina <149749458+angelina0758@users.noreply.github.com>
2025-03-23 14:38:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,26 @@
-//declaration
-var express = require('express');
-var expressSession = require('express-session');
-var bodyParser = require('body-parser');
-var app = express();
-var port = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 3000;
 
-//common controllers
-var signup = require('./controllers/signup');
-var login = require('./controllers/login');
-var logout = require('./controllers/logout');
+const app = express();
+const db = require("./models/db"); // Import database connection
+const homeRoutes = require('./routes/homeRoutes');
+const loginRoutes = require("./routes/loginRoutes");
 
-//admin controllers
-var admin = require('./controllers/admin');
-
-
-//customer controllers
-var customer = require('./controllers/customer');
-
-//configure
-app.set('view engine', 'ejs');
-
-//middlewares
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(expressSession({secret: 'my top secret pass', resave: false, saveUninitialized: true}));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/images', express.static(__dirname + '/images'));
-
-app.use('*', function(req, res, next){
-
-	if(req.originalUrl == '/login' || req.originalUrl == '/signup')
-	{
-		next();
-	}
-	else
-	{
-		if(!req.session.admin && !req.session.customer)
-		{
-			res.redirect('/login');
-			return;
-		}
-		next();
-	}
-});
+// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); // Serve CSS files
+app.set("view engine", "ejs");
 
 
-//routes
-app.use('/login', login);
-app.use('/signup', signup);
-app.use('/logout', logout);
 
-//admin routes
-app.use('/admin', admin);
+// Import Routes
 
+app.use('/', homeRoutes);
+app.use("/login", loginRoutes);
 
-//customer routes
-
-app.use('/customer', customer);
-
-//server start
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
 });
